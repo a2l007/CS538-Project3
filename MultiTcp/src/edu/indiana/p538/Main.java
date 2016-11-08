afdb3aea8400d730a8e5c003d76e7e54c0be9645
va1kyrie <ladyl3na@gmail.com>
2016-11-07 20:15:25 -0500

    added first socket

--- MultiTcp/src/edu/indiana/p538/Main.java
+++ MultiTcp/src/edu/indiana/p538/Main.java
@@ -1,10 +1,30 @@
 package edu.indiana.p538;
 
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Main {
+    private static int PROXY_PORT = 6000;
 
     public static void main(String[] args) {
 	    //number of pipes
         int pipes = Integer.parseInt(args[0]);
+        //classes
+            //parse headers
+        //sockets???
+        //we need a queue for both ends
+
+        //Socket to listen for LP
+        try{
+            InetAddress hostIp = InetAddress.getLocalHost();
+            Socket server = new Socket(hostIp, PROXY_PORT);
+
+            //while socket is listening
+            
+        }catch(Exception e){
+            System.err.println("Invalid host IP address");
+        }
+
 
     }
 }
