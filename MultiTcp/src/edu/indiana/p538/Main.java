f0acd9daab36196921e3b79822b164372aeca91e
va1kyrie <ladyl3na@gmail.com>
2016-11-11 15:05:48 -0500

    working on socket
    (kinda know what i'm doing??)

--- MultiTcp/src/edu/indiana/p538/Main.java
+++ MultiTcp/src/edu/indiana/p538/Main.java
@@ -1,29 +1,37 @@
 package edu.indiana.p538;
 
-import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     private static int PROXY_PORT = 6000;
+    private static int SERVER_PORT = 6001;
 
     public static void main(String[] args) {
-	    //number of pipes
+        //number of pipes
         int pipes = Integer.parseInt(args[0]);
         //classes
-            //parse headers
+        //parse headers
         //sockets???
         //we need a queue for both ends
 
         //Socket to listen for LP
-        try{
-            InetAddress hostIp = InetAddress.getLocalHost();
-            Socket server = new Socket(hostIp, PROXY_PORT);
+        try(
+                ServerSocket proxyServer = new ServerSocket(PROXY_PORT);
+                Socket client = proxyServer.accept();
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(client.getInputStream()));
+        ){
 
-            //while socket is listening
-            
         }catch(Exception e){
-            System.err.println("Invalid host IP address");
+            System.err.println("Error opening socket");
         }
+
+        //while socket is listening
 
 
     }
